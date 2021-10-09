fcb984f8cd95b4a3bd1ddff52e2c4ca5a592564d
root <root@67552d25389d>
2021-10-09 18:10:06 +0000

    Minion rest

--- src/_05_minion/MinionTest.java
+++ src/_05_minion/MinionTest.java
@@ -1,4 +1,3 @@
-// hello
 package _05_minion;
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2019
@@ -12,23 +11,19 @@ import org.junit.Test;
 
 
 /*
-
 1. 
 Run this MinionTest class. Notice the JUnit tab (by the Package Explorer). It shows a RED bar, because the tests do
 not run successfully. Check the Failure Trace below that shows "Minion cannot be resolved to a type". 
 This is because there is no Minion class in the package.
-
 2.
  So to make these tests pass, you will first need to create a Minion class with the member variables below:
-
   			private String name; 
   			private int eyes; 
   			private String color; 
   			private String master;
-
  3.
   Next, create a constructor, and getters and setters for all the member variables of the Minion class. 
-  If they’re done right, all these tests will pass.
+  If theyre done right, all these tests will pass.
   
   To check, run this MinionTest class and the JUnit tab should show a GREEN bar.
     
@@ -61,6 +56,5 @@ public class MinionTest {
 	}
 
 }
-
-
+ 
 
